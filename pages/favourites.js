c193520b086822bd1f01095d5b3eb606e0ee4dfe
saihajvir <saihajvir@hotmail.com>
2022-02-18 18:37:47 -0800

    can add something to favs in localStorage, not appearing on favs page yet

---
+++ pages/favourites.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+import Router, { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+
+import Background from '../comps/Background'
+import Recipe from '../comps/Recipe'
+import Card from '../comps/Card'
+import clientPromise from '../lib/mongodb'
+
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content: center;
+`
+const Spacer = styled.div`
+    width: 100%;
+    height: 20vh;
+`
+
+export default function Favourites({recipes}){
+
+    const [storage, setStorage] = useState()
+
+    useEffect(() => {
+        const curRec = recipes
+        console.log(curRec)
+        setRec(curRec);
+    
+        setStorage(localStorage.getItem("recipe_id", id))
+    }, [])
+    
+    const [rec, setRec] = useState([]);
+    const r = useRouter();
+    const { id } = r.query
+
+    return <>      
+        <Background/>
+            <Wrapper>
+                <Spacer/>
+                   {console.log(storage)}
+                <Card/>
+            </Wrapper>
+
+    </>
+}
+
+
+export async function getServerSideProps(context) {
+    const client = await clientPromise;
+  
+    const db = client.db("recipesDB");
+  
+    let recipes = await db.collection("recipes").find({}).limit(110).toArray();
+    recipes = JSON.parse(JSON.stringify(recipes));
+  
+    return {
+      props: { recipes },
+    };
+  }
+  
